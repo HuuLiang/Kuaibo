9c44bbad98efde40dd99b9591fad55f9c9c8eea8
Zhang Peng <realzzz@gmail.com>
2015-09-01 23:40:03 +0800

    structure commitment.  - initial codes.

---
+++ kuaibov/kuaibov/kbMoreViewController.m
@@ -0,0 +1,37 @@
+//
+//  kbMoreViewController.m
+//  kuaibov
+//
+//  Created by ZHANGPENG on 15/9/1.
+//  Copyright (c) 2015年 kuaibov. All rights reserved.
+//
+
+#import "kbMoreViewController.h"
+
+@interface kbMoreViewController ()
+
+@end
+
+@implementation kbMoreViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
