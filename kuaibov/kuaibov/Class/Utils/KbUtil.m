c8ded313c1d3e022effd78f1d0ee53de5de40c3b
Yu Xulu <tonyfish@qq.com>
2015-09-09 16:25:04 +0800

    Keychain

--- kuaibov/kuaibov/Class/Utils/KbUtil.m
+++ kuaibov/kuaibov/Class/Utils/KbUtil.m
@@ -7,14 +7,24 @@
 //
 
 #import "KbUtil.h"
+#import <SFHFKeychainUtils.h>
+
+static NSString *const kRegisterKeyChainUsername = @"kuaibov_register_username";
+static NSString *const kRegisterKeyChainServiceName = @"kuaibov_register_servicename";
+static NSString *const kRegisterKeyChainPassword = @"kuaibov_register_password";
 
 @implementation KbUtil
 
 + (BOOL)isRegistered {
-    return NO;
+    NSString *registered = [SFHFKeychainUtils getPasswordForUsername:kRegisterKeyChainUsername.md5 andServiceName:kRegisterKeyChainServiceName.md5 error:nil];
+    return registered != nil;
 }
 
 + (void)setRegistered {
-    
+    [SFHFKeychainUtils storeUsername:kRegisterKeyChainUsername.md5
+                         andPassword:kRegisterKeyChainPassword.md5
+                      forServiceName:kRegisterKeyChainServiceName.md5
+                      updateExisting:NO
+                               error:nil];
 }
 @end
