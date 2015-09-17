d80e89e169657328ff470e92fad3ce7d57f0f026
Yu Xulu <tonyfish@qq.com>
2015-09-17 14:55:53 +0800

    Remove dedicated video player view controller.

--- kuaibov/kuaibov/Class/Base/Controller/kbBaseController.m
+++ kuaibov/kuaibov/Class/Base/Controller/kbBaseController.m
@@ -8,7 +8,6 @@
 
 #import "kbBaseController.h"
 #import "KbVideo.h"
-#import "KbVideoPlayViewController.h"
 #import "KbRegisterPopView.h"
 #import "AlipayManager.h"
 #import "KbSystemConfigModel.h"
@@ -16,11 +15,46 @@
 #import "Order.h"
 #import "KbProgram.h"
 
-@interface kbBaseController ()
+@import MediaPlayer;
+@import AVKit;
+@import AVFoundation.AVPlayer;
+@import AVFoundation.AVAsset;
+@import AVFoundation.AVAssetImageGenerator;
 
+@interface kbBaseController ()
+- (UIViewController *)playerVCWithVideo:(KbVideo *)video;
 @end
 
 @implementation kbBaseController
+
+- (UIViewController *)playerVCWithVideo:(KbVideo *)video {
+    UIViewController *retVC;
+    if (NSClassFromString(@"AVPlayerViewController")) {
+        AVPlayerViewController *playerVC = [[AVPlayerViewController alloc] init];
+        playerVC.player = [[AVPlayer alloc] initWithURL:[NSURL URLWithString:video.videoUrl]];
+        [playerVC aspect_hookSelector:@selector(viewDidAppear:)
+                          withOptions:AspectPositionAfter
+                           usingBlock:^(id<AspectInfo> aspectInfo){
+                               AVPlayerViewController *thisPlayerVC = [aspectInfo instance];
+                               [thisPlayerVC.player play];
+                           } error:nil];
+        
+        retVC = playerVC;
+    } else {
+        retVC = [[MPMoviePlayerViewController alloc] initWithContentURL:[NSURL URLWithString:video.videoUrl]];
+    }
+    
+    [retVC aspect_hookSelector:@selector(supportedInterfaceOrientations) withOptions:AspectPositionInstead usingBlock:^(id<AspectInfo> aspectInfo){
+        UIInterfaceOrientationMask mask = UIInterfaceOrientationMaskAll;
+        [[aspectInfo originalInvocation] setReturnValue:&mask];
+    } error:nil];
+    
+    [retVC aspect_hookSelector:@selector(shouldAutorotate) withOptions:AspectPositionInstead usingBlock:^(id<AspectInfo> aspectInfo){
+        BOOL rotate = YES;
+        [[aspectInfo originalInvocation] setReturnValue:&rotate];
+    } error:nil];
+    return retVC;
+}
 
 - (void)viewDidLoad {
     [super viewDidLoad];
@@ -32,10 +66,10 @@
     if (![KbUtil isPaid]) {
         [self showRegisterViewForProgram:program];
     } else if (program) {
-        KbVideoPlayViewController *videoPlayVC = [[KbVideoPlayViewController alloc] initWithVideo:(KbVideo *)program];
+        UIViewController *videoPlayVC = [self playerVCWithVideo:program];
         videoPlayVC.hidesBottomBarWhenPushed = YES;
         //videoPlayVC.evaluateThumbnail = YES;
-        [self.navigationController pushViewController:videoPlayVC animated:YES];
+        [self presentViewController:videoPlayVC animated:YES completion:nil];
     }
 }
 
