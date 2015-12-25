db3a3dfffeada9c3a87413104c7ac7d6f50a8a80
Yu Xulu <tonyfish@qq.com>
2015-12-25 13:41:01 +0800

    Optimize we chat payment check

--- kuaibov/kuaibov/Class/Base/Model/KbWeChatPayQueryOrderRequest.m
+++ kuaibov/kuaibov/Class/Base/Model/KbWeChatPayQueryOrderRequest.m
@@ -17,41 +17,46 @@ static NSString *const kSuccessString = @"SUCCESS";
 
 - (BOOL)queryOrderWithNo:(NSString *)orderNo completionHandler:(KbWeChatPayQueryOrderCompletionHandler)handler {
     
-    srand( (unsigned)time(0) );
-    NSString *noncestr  = [NSString stringWithFormat:@"%d", rand()];
+    dispatch_async(dispatch_get_global_queue(0, 0), ^{
+        srand( (unsigned)time(0) );
+        NSString *noncestr  = [NSString stringWithFormat:@"%d", rand()];
+        
+        NSMutableDictionary *params = @{@"appid":[KbConfig sharedConfig].weChatPayAppId,
+                                        @"mch_id":[KbConfig sharedConfig].weChatPayMchId,
+                                        @"out_trade_no":orderNo,
+                                        @"nonce_str":noncestr}.mutableCopy;
+        //创建支付签名对象
+        payRequsestHandler *req = [[payRequsestHandler alloc] init];
+        //初始化支付签名对象
+        [req init:[KbConfig sharedConfig].weChatPayAppId mch_id:[KbConfig sharedConfig].weChatPayMchId];
+        //设置密钥
+        [req setKey:[KbConfig sharedConfig].weChatPayPrivateKey];
+        //设置回调URL
+        [req setNotifyUrl:[KbConfig sharedConfig].weChatPayNotifyURL];
+        //设置附加数据
+        [req setAttach:[KbConfig sharedConfig].paymentReservedData];
+        
+        NSString *package = [req genPackage:params];
+        NSData *data =[WXUtil httpSend:kWeChatPayQueryOrderUrlString method:@"POST" data:package];
+        
+        XMLHelper *xml  = [[XMLHelper alloc] init];
+        
+        //开始解析
+        [xml startParse:data];
+        
+        NSMutableDictionary *resParams = [xml getDict];
+        self.return_code = resParams[@"return_code"];
+        self.result_code = resParams[@"result_code"];
+        self.trade_state = resParams[@"trade_state"];
+        self.total_fee = ((NSString *)resParams[@"total_fee"]).integerValue / 100;
+        
+        if (handler) {
+            dispatch_async(dispatch_get_main_queue(), ^{
+                handler([self.return_code isEqualToString:kSuccessString] && [self.result_code isEqualToString:kSuccessString], self.trade_state, self.total_fee);
+            });
+        }
+    });
     
-    NSMutableDictionary *params = @{@"appid":[KbConfig sharedConfig].weChatPayAppId,
-                             @"mch_id":[KbConfig sharedConfig].weChatPayMchId,
-                             @"out_trade_no":orderNo,
-                                    @"nonce_str":noncestr}.mutableCopy;
-    //创建支付签名对象
-    payRequsestHandler *req = [[payRequsestHandler alloc] init];
-    //初始化支付签名对象
-    [req init:[KbConfig sharedConfig].weChatPayAppId mch_id:[KbConfig sharedConfig].weChatPayMchId];
-    //设置密钥
-    [req setKey:[KbConfig sharedConfig].weChatPayPrivateKey];
-    //设置回调URL
-    [req setNotifyUrl:[KbConfig sharedConfig].weChatPayNotifyURL];
-    //设置附加数据
-    [req setAttach:[KbConfig sharedConfig].paymentReservedData];
-    
-    NSString *package = [req genPackage:params];
-    NSData *data =[WXUtil httpSend:kWeChatPayQueryOrderUrlString method:@"POST" data:package];
-
-    XMLHelper *xml  = [[XMLHelper alloc] init];
-    
-    //开始解析
-    [xml startParse:data];
-    
-    NSMutableDictionary *resParams = [xml getDict];
-    self.return_code = resParams[@"return_code"];
-    self.result_code = resParams[@"result_code"];
-    self.trade_state = resParams[@"trade_state"];
-    self.total_fee = ((NSString *)resParams[@"total_fee"]).integerValue / 100;
-    
-    if (handler) {
-        handler([self.return_code isEqualToString:kSuccessString] && [self.result_code isEqualToString:kSuccessString], self.trade_state, self.total_fee);
-    }
     return YES;
 }
 @end
